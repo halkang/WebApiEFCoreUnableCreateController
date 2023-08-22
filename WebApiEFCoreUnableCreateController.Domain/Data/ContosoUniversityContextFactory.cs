510991423d651d2b6451a2357dc6af9c6422d6f9
halkang <cmhitdev@gmail.com>
2023-08-22 10:47:02 +0800

    加入ContosoUniversityContextFactory

---
+++ WebApiEFCoreUnableCreateController.Domain/Data/ContosoUniversityContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WebApiEFCoreUnableCreateController.Domain.Data;
+
+public partial class ContosoUniversityContextFactory : IDesignTimeDbContextFactory<ContosoUniversityContext>
+{
+    public ContosoUniversityContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ContosoUniversityContext>();
+
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ContosoUniversity;Integrated Security=True");
+
+        return new ContosoUniversityContext(optionsBuilder.Options);
+    }
+}
